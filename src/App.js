065711e6ec337f6e6ba18e0328e079c1052765b8
Kepa-dev <lukaszkepa03@gmail.com>
2023-12-04 18:58:01 +0100

    tailwind added

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import logo from './logo.svg';
+import './App.css';
+export default function App() {
+  return(
+    <p className='text-9xl font-bold h-screen bg-beige text-navy'>hello world</p>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      'darkNavy' : '#01101f',
+      'navy': '#05386B',
+      'darkGreen': '#379683',
+      'green': '#5CDB95',
+      'mint': '#8EE4AF',
+      'beige': '#EDF5E1',
+      'white' : '#FFF',
+      'black' : '#000',
+    },
+  },
+  plugins: [],
+}

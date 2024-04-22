cf42ea350b49aa381436173287bb77d3d0a1d14b
kepa-lukasz <lukaszkepa03@gmail.com>
2024-04-22 23:49:00 +0200

    email function added

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -1,22 +1,79 @@
+import React, { useRef } from "react";
+import emailjs from '@emailjs/browser'
+import { Button } from "react-bootstrap";
+import AtomButton from "../../components/another/atom-button";
 const ContactForm = () => {
+
+    const email = useRef()
+    const text = useRef()
+    const title = useRef()
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        const email_params = {
+            from_name: "email.current.value",
+            to_name: "Łukasz",
+            message: "text.current.innerHTML",
+            title: "title.current.value"
+        }
+
+        emailjs
+            .sendForm('service_qzp2rwj', 'template_z4x6tev', form.current, {
+                publicKey: 'niSLg959mx5rx5ZeC',
+            })
+            .then(
+                () => {
+                    console.log('SUCCESS!');
+                },
+                (error) => {
+                    console.log('FAILED...', error.text);
+                },
+            );
+    };
+
     return (
-        
+        <div className="w-100" style={{ maxWidth: "1900px" }}>
+            <p id="contact"></p>
+            <h1 className="pt-12 ms-6 dark:text-white ps-4 text-4xl my-4 font-bold">Kontakt</h1>
+            <div className="underline ms-12 bg-darkGreen dark:bg-lightLemon mb-3"></div>
+            <h2 style={{ maxWidth: "80vw" }} className="ms-6 ps-4 fs-5 dark:text-white ">Jeśli Masz jakies pytania, napisz do mnie!</h2>
 
-            <div className="w-full" style={{maxWidth : "1900px"}}>
-                <p id="contact"></p>
-                <h1 className="pt-12 ms-6 dark:text-white ps-4 text-4xl my-4 font-bold">Kontakt</h1>
-                <div className="underline ms-12 bg-darkGreen dark:bg-lightLemon mb-3"></div>
-                <h2 className="ms-6 ps-4 fs-5 dark:text-white">Jeśli masz jakieś pytania, napisz do mnie</h2>
-                <div className="w-full justify-content-center d-flex">
+            <div className="d-flex justify-content-center w-100   my-5  py-3">
+
+                <form className="mx-5  dark:text-white dark:bg-darkGrey bg-fiolet py-4 px-1 px-sm-4  rounded-3 w-100"
+                    ref={form}
+                onSubmit={sendEmail}
+                >
+                    <div className="w-100 d-flex flex-wrap " >
 
 
-                    <div className="bg-white p-2 col-12 col-10 mb-12" style={{ maxWidth: "1000px" }}>
-
-                        <iframe title="contact form" src="https://www.cognitoforms.com/f/4sAj-d0vkkSpgxh2b_acLQ/1" style={{ padding: "10px", width: "100%" }} height="493"></iframe>
+                        <div class="mb-3 col-12 col-md-6 px-2">
+                            <label for="emailinput" className="form-label fs-5 ">Adres email</label>
+                            <input required name="from_name" type="email" className="form-control" id="emailinput" placeholder="name@example.com" />
+                        </div>
+                        <div class="mb-3 col-12 col-md-6 px-2">
+                            <label for="title" className="form-label fs-5">Tytuł</label>
+                            <input required name="title" type="text" className="form-control" id="title" placeholder="tytuł" />
+                        </div>
                     </div>
-                </div>
-            </div>
+                    <div class="mb-3 px-2">
+                        <label for="textarea" className="form-label">Twoja wiadomość</label>
+                        <textarea required placeholder="twoja wiadomość" name="message" class="form-control" id="textarea" rows="3"></textarea>
+                    </div>
+                    <div className="w-100 px-2 d-flex mb-2">
+                        <input style={{scale: "1.4"}} required type="checkbox" id="confirm" className="me-3 text-top" />
+                        <label for="confirm">Wyrażam zgodę na przetwarzanie danych osobowych zgodnie z ustawą o ochronie danych osobowych w związku z wysłaniem zapytania przez formularz kontaktowy. Podanie danych jest dobrowolne, ale niezbędne do przetworzenia zapytania. Zostałem poinformowany, że przysługuje mi prawo dostępu do swoich danych, możliwości ich poprawiania, żądania zaprzestania ich przetwarzania przez wysłanie wiadomości e-mail na adres lukaszkepa2003@gmail.com. Administratorem danych osobowych jest Łukasz Kępa</label>
+                    </div>
+                    <div>
 
-    )
-}
+                        <AtomButton text="Wyślij wiadomość" />
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+};
 export default ContactForm
--- src/pages/hobbies/myhobbies.js
+++ src/pages/hobbies/myhobbies.js
@@ -13,47 +13,47 @@ import { EffectCoverflow, Pagination } from 'swiper/modules';
 
 const Hobbies = () => {
     return (
-        <div className="w-full m-0 p-0 mb-5" style={{ maxWidth: "1900px", overflow : "hidden" }}>
+        <div className="w-full m-0 p-0 mb-5" style={{ maxWidth: "1900px", overflow: "hidden" }}>
             <p id="hobby"></p>
             <h1 className="pt-12 ms-6 dark:text-white ps-4 text-4xl my-4 font-bold">Hobby</h1>
             <div className="underline ms-12 bg-darkGreen dark:bg-lightLemon mb-3"></div>
-            <h2 className="ms-6 ps-4 fs-5 dark:text-white ">W tej sekcji napisałem, co lubię robić w wolnych chwilach</h2>            
+            <h2 className="ms-6 ps-4 fs-5 dark:text-white ">W tej sekcji napisałem, co lubię robić w wolnych chwilach</h2>
             <div className='d-flex justify-content-center'>
-            <div className="swiper-con" style={{}}>
-                <Swiper
-                loop={true}
-                    navigation={true}
-                    effect={'coverflow'}
-                    grabCursor={true}
-                    centeredSlides={true}
-                    slidesPerView={'auto'}
-                    coverflowEffect={{
-                        rotate: 50,
-                        stretch: 0,
-                        depth: 100,
-                        modifier: 1,
-                        slideShadows: true,
-                    }}
-                    pagination={true}
-                    modules={[EffectCoverflow, Pagination]}
-                    className="mySwiper"
-                >
+                <div className="swiper-con" style={{}}>
+                    <Swiper
+                        loop={true}
+                        navigation={true}
+                        effect={'coverflow'}
+                        grabCursor={true}
+                        centeredSlides={true}
+                        slidesPerView={'auto'}
+                        coverflowEffect={{
+                            rotate: 50,
+                            stretch: 0,
+                            depth: 100,
+                            modifier: 1,
+                            slideShadows: true,
+                        }}
+                        pagination={true}
+                        modules={[EffectCoverflow, Pagination]}
+                        className="mySwiper"
+                    >
                         <SwiperSlide>
-                            <img src={require("../../images/hobbies/web.avif")} alt="napis WEB utowrzony z wielu znaków"/>
+                            <img src={require("../../images/hobbies/web.avif")} alt="napis WEB utowrzony z wielu znaków" />
                             <div className='slide-text d-flex justify-content-end'>
                                 <h2 className='w-full fs-2 text-white text-center'>Web Development</h2>
                                 <p className='px-3 px-md-5 text-center text-white fs-5'>od kilku lat interesuje się programowaniem internetowym. Staram się nabierać doświadczenie i kształcić się przy pomocy kursów, webinarów i pracy własnej</p>
                             </div>
                         </SwiperSlide>
                         <SwiperSlide>
-                            <img src={require("../../images/hobbies/mozg.avif")} alt="wiele zer i jedynek, kod binarny"/>
+                            <img src={require("../../images/hobbies/mozg.avif")} alt="wiele zer i jedynek, kod binarny" />
                             <div className='slide-text d-flex justify-content-end'>
                                 <h2 className='w-full fs-2 text-white text-center'>Gry logiczne</h2>
                                 <p className='px-3 px-md-5 text-center text-white fs-5'>Co jakiś czas znajdują nową grę logiczną lub inną aktywność, na którą poświęcam wolne chile(pasjans, sudoku, szachy, kraje świata)</p>
                             </div>
                         </SwiperSlide>
                         <SwiperSlide>
-                            <img src={require("../../images/hobbies/pilka.avif")}alt="sztuczka piłkarska" />
+                            <img src={require("../../images/hobbies/pilka.avif")} alt="sztuczka piłkarska" />
                             <div className='slide-text d-flex justify-content-end'>
                                 <h2 className='w-full fs-2 text-white text-center'>Sport</h2>
                                 <p className='px-3 px-md-5 text-center text-white fs-5'>Wieczorami staram się regularnie wychodzić na siłownię lub orlik, pomaga mi to utrzymywać względną formę fizyczną :)</p>
@@ -77,7 +77,7 @@ const Hobbies = () => {
                     </Swiper>
                 </div >
             </div>
-            </div>
+        </div>
     );
 }
 export default Hobbies;
